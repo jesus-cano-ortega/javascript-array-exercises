0ab8857589fd2875fef02b80cae2640ee53667b3
Jesús Cano <jcanort@gmail.com>
2021-09-26 13:11:37 +0000

    Array Exercise

--- exercises/18-Nested-arrays/app.js
+++ exercises/18-Nested-arrays/app.js
@@ -1,1 +1,11 @@
 var coordinatesArray = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]];
+
+let output = [];
+
+for (let index in coordinatesArray){
+    output.push(coordinatesArray[index][1]);
+}
+
+for (let index = 0; index < output.length; index++) {
+    console.log(output[index]);
+}
